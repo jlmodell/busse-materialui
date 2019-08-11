9f07c40ce95d35112e285a7950d7b9d629b4940a
Jeff Modell <modell.jeff@me.com>
2019-08-11 07:11:45 -0400

    small commits

--- src/pages/Sales.js
+++ src/pages/Sales.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 import SetDates from "../components/SetDates";
 import Tables from "../components/Tables";
 
-import { fetchSalesData, fetchIndividualSalesByCustomer } from '../store/actions'
+import {
+  fetchSalesData,
+  fetchIndividualSalesByCustomer
+} from "../store/actions";
+
+const useStyles = makeStyles(theme => ({
+  progress: {
+    margin: theme.spacing(2)
+  },
+  loaderDiv: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+}));
 
 const columns = [
   {
@@ -11,7 +28,7 @@ const columns = [
     label: "Customers",
     options: {
       filter: true,
-      sort: true,
+      sort: true
     }
   },
   {
@@ -51,33 +68,54 @@ const columns = [
   }
 ];
 
-const Sales = (props) => {
-  const { fetchSalesData } = props
+const Sales = props => {
+  const { fetchSalesData } = props;
+  const classes = useStyles();
 
   const options = {
     filter: true,
     filterType: "dropdown",
     responsive: "stacked",
     onRowClick: (rowData, rowState) => {
-      props.fetchIndividualSalesByCustomer(rowData[1])
+      props.fetchIndividualSalesByCustomer(rowData[1]);
       props.history.push({
-          pathname: `/indivbycust`,
-          state: {cid: rowData[1], cname: rowData[0]}
-      })
+        pathname: `/indivbycust`,
+        state: { cid: rowData[1], cname: rowData[0] }
+      });
     }
+  };
+
+  const renderLoader = () => {
+    return (
+      <div className={classes.loaderDiv}>
+        <CircularProgress className={classes.progress} />
+      </div>
+    );
   };
 
   return (
     <div>
       <SetDates />
       <button onClick={fetchSalesData}>Fetch Data</button>
-      {props.sales.loading ? "Loading..." : <Tables options={options} columns={columns} tableName="Sales" data={props.sales.customerDetails} />}
+      {props.sales.loading ? (
+        renderLoader()
+      ) : (
+        <Tables
+          options={options}
+          columns={columns}
+          tableName='Sales'
+          data={props.sales.customerDetails}
+        />
+      )}
     </div>
   );
-}
+};
 
-const mapStateToProps = ({sales}) => {
-  return { sales }
-}
+const mapStateToProps = ({ sales }) => {
+  return { sales };
+};
 
-export default connect(mapStateToProps, { fetchSalesData, fetchIndividualSalesByCustomer })(Sales)
+export default connect(
+  mapStateToProps,
+  { fetchSalesData, fetchIndividualSalesByCustomer }
+)(Sales);
