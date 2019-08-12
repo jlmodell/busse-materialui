848dcb18536f47427483bb40bc9d36d61c1e6b37
Jeff Modell <modell.jeff@me.com>
2019-08-12 07:53:14 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,75 @@
 import React from "react";
 
 export default function Home() {
+  const backend = "Built with nestjs <typescript>";
+  const frontend = [
+    "react <framework>",
+    "react-router-dom <routing>",
+    "redux & react-redux <state management>",
+    "@material-ui/core & @material-ui/icons <component elements>",
+    "@MuiPicker <date picker>",
+    "MUIDataTable <tables>"
+  ];
+  const data = [
+    {
+      version: "0.5.0",
+      description:
+        "Added table component from Material UI for fetched tabular data",
+      frontend: frontend.slice(0, 5),
+      backend
+    },
+    {
+      version: "0.4.0",
+      description: "Added date picker element from Muipicker",
+      frontend: frontend.slice(0, 4),
+      backend
+    },
+    {
+      version: "0.3.0",
+      description: "Added components from Material UI for layout",
+      frontend: frontend.slice(0, 3),
+      backend
+    },
+    {
+      version: "0.2.0",
+      description: "Added redux for state management",
+      frontend: frontend.slice(0, 2),
+      backend
+    },
+    {
+      version: "0.1.0",
+      description: "Added sales & items component",
+      frontend: frontend.slice(0, 1),
+      backend
+    },
+    {
+      version: "0.0.0",
+      description: "Initial commit",
+      frontend: frontend.slice(0, 1),
+      backend
+    }
+  ];
+
+  const styles = {
+    li: {
+      margin: "3rem 10rem"
+    },
+    title: {
+      margin: "2rem 10rem"
+    }
+  };
+
   return (
     <div>
-      <h1>Busse Hospital Disposables</h1>
+      <h1 style={styles.title}>Notes:</h1>
+      <ul>
+        {data.map((v, i) => (
+          <li key={i} style={styles.li}>
+            <b>{v.version}</b>: <i>{v.description}</i> [{v.frontend},{" "}
+            {v.backend}]
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
